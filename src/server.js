309e1fbdfc8e1d7a7869296f8cb54ddf27a018cd
Victor Pragana <victormerigue@gmail.com>
2019-09-06 11:40:49 -0300

    upload local salvando no banco de dados

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/upload", {
+  useNewUrlParser: true
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use(require("./routes"));
+
+app.listen(5000);
